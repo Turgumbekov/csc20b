556add8ff64de9b5d13fa840632818df59bb0769
Turgumbekov <93367245+Turgumbekov@users.noreply.github.com>
2021-11-05 03:37:54 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 <html>
 <head>
-<title>Page Title</title>
+<style>
+/* This style sets the width of all images to 100%: */
+img {
+  width: 100%;
+}
+</style>
 </head>
 <body>
 
-<h1>Temirlan SMM</h1>
-<p> I can fix any your problem .</p>
+<h2>Width/Height Attributes or Style?</h2>
+
+<p>The first image uses the width attribute (set to 128 pixels), but the style in the head section overrides it, and sets the width to 100%.</p>
+
+<img src="https://wroom.ru/i/cars2/mercedesbenz_amggt_1.jpg" alt="HTML5 Icon" width="830" height="472">
+
+<p>The second image uses the style attribute to set the width to 128 pixels, this will not be overridden by the style in the head section:</p>
+
+<img src="https://wroom.ru/i/cars2/mercedesbenz_amggt_1.jpg"  style="width:430px;height:272px;">
 
 </body>
 </html>
