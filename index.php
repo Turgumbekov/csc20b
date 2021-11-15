c53e8f52b69e0ccaf16ad5f46a77c861cbd077b9
Turgumbekov <93367245+Turgumbekov@users.noreply.github.com>
2021-11-15 12:48:27 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,46 @@
-<!DOCTYPE html>    
-<html>    
-<head>    
-    <title>Login Form</title>    
-    <link rel="stylesheet" type="text/css" href="css/style.css">    
-</head>    
-<body>    
-    <h2>Login Page</h2><br>    
-    <div class="login">    
-    <form id="login" method="get" action="login.php">    
-        <label><b>User Name     
-        </b>    
-        </label>    
-        <input type="text" name="Uname" id="Uname" placeholder="Username">    
-        <br><br>    
-        <label><b>Password     
-        </b>    
-        </label>    
-        <input type="Password" name="Pass" id="Pass" placeholder="Password">    
-        <br><br>    
-        <input type="button" name="log" id="log" value="Log In Here">       
-        <br><br>    
-        <input type="checkbox" id="check">    
-        <span>Remember me</span>    
-        <br><br>    
-        Forgot <a href="#">Password</a>    
-    </form>     
-</div>    
-</body>    
-</html>     
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login Form</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
 <style>
-body {
-  background-color:blue;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-size: 100% 100%;
-}
+    body {
+        background-color: blue;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: 100% 100%;
+    }
+    
+    center {
+        margin-top: 20%;
+    }
 </style>
+
+<body>
+    <center>
+        <h2>Login Page</h2><br>
+        <div class="login">
+            <form id="login" method="get" action="login.php">
+                <label><b>User Name     
+        </b>    
+        </label>
+                <input type="text" name="Uname" id="Uname" placeholder="Username">
+                <br><br>
+                <label><b>Password     
+        </b>    
+        </label>
+                <input type="Password" name="Pass" id="Pass" placeholder="Password">
+                <br><br>
+                <input type="button" name="log" id="log" value="Log In Here">
+                <br><br>
+                <input type="checkbox" id="check">
+                <span>Remember me</span>
+                <br><br> Forgot <a href="#">Password</a>
+            </form>
+        </div>
+    </center>
+</body>
+
+</html>
