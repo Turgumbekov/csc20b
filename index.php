678e8ceffddfb243f1a3ff1f879b1ae2e2301182
Turgumbekov <93367245+Turgumbekov@users.noreply.github.com>
2021-11-15 12:42:24 +0600

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 </html>     
 <style>
 body {
-  background-image: url('img_girl.jpg');
+  background-color:blue;
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-size: 100% 100%;
